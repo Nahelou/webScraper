3bc99cc1b9debfdb455066ef7a9df56a2725b818
Quentin Nahélou <nahelouquentin@gmail.com>
2020-09-24 23:41:45 +0200

    add search form

--- cli/src/components/WebScraper.vue
+++ cli/src/components/WebScraper.vue
@@ -4,6 +4,7 @@
       <div class="card-content">
         <div class="field">
           <div class="control">
+            <h2 class="subtitle">Site Open Data</h2>
             <div class="select is-medium">
               <select>
                 <option  v-for="website in openDataWebsites" :key="website._id">{{website.name}}</option>
@@ -12,6 +13,16 @@
           </div>
         </div>
       </div>
+<div class="field is-grouped mx-5">
+  <p class="control is-expanded">
+    <input class="input" type="text" placeholder="Search keyword">
+  </p>
+  <p class="control">
+    <a class="button is-info">
+      Search
+    </a>
+  </p>
+</div>
     </div>
   </div>
 </template>
@@ -87,79 +98,15 @@ export default {
   },
   setup() {
 
-    const sites = [
-      {
-        id: 1,
-        name: "OpenDataSoft",
-        url:
-          "https://data.opendatasoft.com/explore/?disjunctive.language&disjunctive.source_domain_title&disjunctive.theme&disjunctive.semantic.classes&disjunctive.semantic.properties&sort=explore.popularity_score",
-        classSearchParent: "ods-catalog-card__wrapper",
-        classSearchChild: "ods-catalog-card",
-        title: "gotham-book ng-binding",
-        logo: "centralstore-catalog-card__portal-icon",
-      },
-      {
-        id: 2,
-        name: "Kaggle",
-        url: "https://www.kaggle.com/datasets?search=",
-        classSearchParent: "sc-pQFHc biYjWK",
-        classSearchChild: "sc-oVcRo sc-qYUgX eZPPVU sc-pkryX eJxnfX",
-        title: "sc-Axmtr sc-fzoiQi sc-oTDcV fBazUz",
-        logo: "sc-pJjas gWUsWA",
-      },
-      {
-        id: 3,
-        name: "DataGouv",
-        url: "https://www.data.gouv.fr/fr/search/?q=",
-        classSearchParent: "search-results",
-        classSearchChild: "search-result dataset-result",
-        title: "result-title ellipsis",
-        logo: "result-logo pull-left",
-      },
-      {
-        id: 4,
-        name: "Data World",
-        url:
-          "https://data.world/search?context=community&page=1&q=fish&type=data",
-        classSearchParent: "PaginatedList__noChromeList___3wrVT list-group",
-        classSearchChild:
-          "Card__card___aSnPt Card__linked___2yUGm Card__chrome___viUm2",
-        title: "Card__title___2kymQ",
-        logo: "result-logo pull-left",
-      },
-      {
-        id: 5,
-        name: "OpenData Paris",
-        url:
-          "https://opendata.paris.fr/explore/?disjunctive.theme&disjunctive.publisher&disjunctive.keyword&disjunctive.modified&disjunctive.features&sort=modified",
-        classSearchParent: "ods-result-list odswidget-infinite-scroll-results",
-        classSearchChild: "ods-catalog-card",
-        title: "ods-catalog-card__title ng-binding",
-        logo:
-          "ods-svginliner__svg-container ods-svginliner__svg-container--colorless",
-      },
-      {
-        id: 6,
-        name: "Open Data Rennes Metropole",
-        url: "https://data.rennesmetropole.fr/explore/?sort=modified",
-        classSearchParent: "ods-result-list odswidget-infinite-scroll-results",
-        classSearchChild: "ods-catalog-card",
-        title: "ods-catalog-card__title ng-binding",
-        logo: "ods-svginliner__svg-container",
-      },
-    ];
-    // openDataWebsites.value = sites.json();
     const bars = ref([]);
     const API_URL = "http://localhost:4242/api/v1/bars";
 
     async function getBars() {
       const response = await fetch(API_URL);
       const json = await response.json();
-      openDataWebsites.value = sites;
     }
 
     getBars();
-    console.log(bars);
 
     return {
       bars,
